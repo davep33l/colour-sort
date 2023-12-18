13048441271ff4a70611d782769a44f8bf8f502a
dave peel <davidpeel33@hotmail.com>
2023-12-18 10:14:59 +0000

    Add Game class with initial properties/methods
    This commit adds in a Game class which will house all the logic for
    starting a new Game object.
    
    This commit sets 3 base properties and the associated methods.
    
    There is a baseStackAmt property which is defaulted to 4 as this is the
    minumum in which the game will be enjoyable.
    There are associated get, set and update methods for baseStackAmt, with
    the logic in the update method checking for incorrect data types if used
    and not allowing a number below the default to be passed in.
    
    There is a baseBlockAmt property which is defaulted to 4 as this is the
    minumum in which the game will be enjoyable.
    There are associated get, set and update methods for baseBlockAmt, with
    the logic in the update method checking for incorrect data types if used
    and not allowing a number below the default to be passed in.
    
    There is a baseEmptyStackAmt property which is defaulted to 4 as this is the
    minumum in which the game will be enjoyable.
    There are associated get, set and update methods for baseEmptyStackAmt, with
    the logic in the update method checking for incorrect data types if used
    and not allowing a number below the default to be passed in.
    
    This has been set up in this way to allow for any gameplay changes to be
    made, as these values will drive the remaining game logic.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,135 @@
-window.onload = checkLoaded();
+class Game {
+    constructor() {
 
-function checkLoaded() {
+        // ##################################
+        // # GAME INITIALISATION PROPERTIES #
+        // ##################################
+        this.baseStackAmt = 4; // the game has to start with at least 4 stacks to be playable/enjoyable
+        this.baseBlockAmt = 4; // the game has to start with at least 4 blocks in each stack to be playable/enjoyable
+        this.baseEmptyStackAmt = 2; // the game has to start with at least 2 empty stacks to have a chance of completing the game
+
+    }
+    // ###############################
+    // # GAME INITIALISATION METHODS #
+    // ###############################
+    getBaseStackAmt() {
+        return this.baseStackAmt;
+    }
+
+    setBaseStackAmt(integer) {
+        this.baseStackAmt = integer;
+    }
+
+    updateBaseStackAmt(integer) {
+
+        console.log(typeof integer)
+        console.log(Number.isInteger(integer))
+
+        /* 
+            The below code is for protection against input of an incorrect data type, 
+            as only an integer can be passed into the function. If an incorrect type 
+            is determined, the following code will return false, which leaves the 
+            baseStackAmt property set at its default value. 
+        */
+        if (!Number.isInteger(integer)) {
+            console.log("not a number");
+            return;
+        }
+
+        /*
+             When initialising the game you can set the baseStackAmt, the below
+             checks the input value is a minimum of the default baseStackAmt and 
+             only updates to the value passed in if it is greater than the baseStackAmt.
+        */
+        if (integer < this.baseStackAmt) {
+            this.setBaseStackAmt(this.baseStackAmt);
+        } else {
+            this.setBaseStackAmt(integer);
+        }
+    }
+
+    getBaseBlockAmt() {
+        return this.baseBlockAmt;
+    }
+
+    setBaseBlockAmt(integer) {
+        this.baseBlockAmt = integer;
+    }
+
+    updateBaseBlockAmt(integer) {
+
+        console.log(typeof integer)
+        console.log(Number.isInteger(integer))
+
+        /* 
+            The below code is for protection against input of an incorrect data type, 
+            as only an integer can be passed into the function. If an incorrect type 
+            is determined, the following code will return false, which leaves the 
+            baseBlockAmt property set at its default value. 
+        */
+        if (!Number.isInteger(integer)) {
+            console.log("not a number");
+            return;
+        }
+
+        /*
+             When initialising the game you can set the baseBlockAmt, the below
+             checks the input value is a minimum of the default baseBlockAmt and 
+             only updates to the value passed in if it is greater than the baseBlockAmt.
+        */
+        if (integer < this.baseBlockAmt) {
+            this.setBaseBlockAmt(this.baseBlockAmt);
+        } else {
+            this.setBaseBlockAmt(integer);
+        }
+    }
+
+    getBaseEmptyStackAmt() {
+        return this.baseEmptyStackAmt;
+    }
+
+    setBaseEmptyStackAmt(integer) {
+        this.baseEmptyStackAmt;
+    }
+
+    updateBaseEmptyStackAmt(integer) {
+
+        console.log(typeof integer)
+        console.log(Number.isInteger(integer))
+
+        /* 
+            The below code is for protection against input of an incorrect data type, 
+            as only an integer can be passed into the function. If an incorrect type 
+            is determined, the following code will return false, which leaves the 
+            baseEmptyStackAmt property set at its default value. 
+        */
+        if (!Number.isInteger(integer)) {
+            console.log("not a number");
+            return;
+        }
+
+        /*
+             When initialising the game you can set the baseEmptyStackAmt, the below
+             checks the input value is a minimum of the default baseEmptyStackAmt and 
+             only updates to the value passed in if it is greater than the baseEmptyStackAmt.
+        */
+        if (integer < this.baseEmptyStackAmt) {
+            this.setBaseEmptyStackAmt(this.baseEmptyStackAmt);
+        } else {
+            this.setBaseEmptyStackAmt(integer);
+        }
+    }
+}
+
+// Checks for the window to have loaded before running the game initialisation
+window.onload = hasLoaded();
+
+// Game initialisation function which creates a new Game object, where the
+// base properties can be set. 
+function hasLoaded() {
     console.log("The window has loaded")
+    newGame = new Game();
+    newGame.updateBaseStackAmt(4);
+    newGame.updateBaseBlockAmt(5);
+    newGame.updateBaseEmptyStackAmt(2);
 }
