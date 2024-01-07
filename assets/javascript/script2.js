d13c43f4f3b7bc58a5cd1f1d716df86570b24256
dave peel <davidpeel33@hotmail.com>
2024-01-07 17:16:18 +0000

    Add ColourInitialiser class
    This commit adds a new class called ColourInitialiser. It is a refactor
    of a few methods that were part of the original Game class.
    
    The change with the method vs the previous method is that now it
    generates a full array of items, both colours needed and undefined
    values for none coloured blocks. Which makes it easier to assign the
    colours for the game.
    
    The method also contains error checking to assist the developer, by
    throwing errors if incorrect data types are passed as parameters.
    
    The functionality in general remains the same. It takes in the
    parameters of how many stacks the game needs, how many blocks per stack
    and how many empty stacks there should be.
    
    It creates an array of colours and randomly selects them to create the
    starting colours then appends to that array the correct amount of
    undefined colour values for the block.

--- assets/javascript/script2.js
+++ assets/javascript/script2.js
@@ -46,6 +46,9 @@ class GameManager {
         this.levelManager = new LevelManager();
         this.currentLevel = 1;
 
+        this.colourInitialiser = new ColourInitialiser();
+        this.initialColourArray = []
+
     }
 
     startGame() {
@@ -55,6 +58,9 @@ class GameManager {
 
         const startingStackAmountForLevel = this.levelManager.getStartingStackAmtForLevel(this.currentLevel, this.gameSettings.stackAmt)
         console.log(startingStackAmountForLevel)
+
+        this.initialColourArray = this.colourInitialiser.setInGameColours(startingStackAmountForLevel, this.gameSettings.blockAmt, this.gameSettings.emptyStackAmt)
+        console.log(this.initialColourArray)
 
         console.log(this)
 
@@ -120,7 +126,60 @@ class GameTitle {
 
 class ColourInitialiser {
     constructor() {
+        this.BASE_COLOURS = [
+            '#e6194B', // Red
+            '#3cb44b', // Green
+            '#4363d8', // Blue
+            '#ffe119', // Yellow
+            '#f58231', // Orange
+            '#911eb4', // Purple
+            '#42d4f4', // Cyan
+            '#f032e6', // Magenta
+            '#800000', // Maroon
+            '#a9a9a9', // Grey
+        ];
+    }
 
+    setInGameColours(normStack, normBlock, emptyStack) {
+
+        if (typeof normStack != "number") {
+            throw new Error(`Incorrect data type. Needs to be a number. You passed in a ${typeof normStack} of "${normStack}" for normStack`)
+        }
+
+        if (typeof normBlock != "number") {
+            throw new Error(`Incorrect data type. Needs to be a number. You passed in a ${typeof normBlock} of "${normBlock}" for normBlock`)
+        }
+
+        if (typeof emptyStack != "number") {
+            throw new Error(`Incorrect data type. Needs to be a number. You passed in a ${typeof emptyStack} of "${emptyStack}" for emptyStack`)
+        }
+
+        let coloursRequired = 0;
+        coloursRequired = (normStack - emptyStack) * normBlock;
+        let tempArray1 = [];
+        for (let i = 0; i < normStack - emptyStack; i++) {
+            for (let j = 0; j < normBlock; j++) {
+                tempArray1.push(this.BASE_COLOURS[i]);
+            }
+        }
+
+        let tempArray2 = [];
+        for (let i = 0; i < coloursRequired; i++) {
+            let multiplier = 0;
+            multiplier = tempArray1.length;
+            let randomNumber = 0;
+            randomNumber = Math.floor(Math.random() * multiplier);
+            tempArray2.push(tempArray1[randomNumber]);
+            tempArray1.splice(randomNumber, 1);
+        }
+
+        for (let i = 0; i < normStack - emptyStack; i++) {
+            for (let j = 0; j < normBlock; j++) {
+                tempArray2.push(undefined);
+            }
+        }
+
+        return tempArray2;
     }
 }
 
