2751bf1d88185c07ea793078e02ae6e3a7333c3e
dave peel <davidpeel33@hotmail.com>
2024-01-07 16:40:10 +0000

    Created base classes
    This commit adds the base classes to be used as part of the refactor.

---
+++ assets/javascript/script2.js
@@ -0,0 +1,42 @@
+class GameSettings {
+    constructor() {
+
+    }
+}
+
+class LevelManager {
+    constructor() {
+
+    }
+}
+
+class GameManager {
+    constructor() {
+
+    }
+}
+
+class GameTitle {
+    constructor() {
+
+    }
+}
+
+class ColourInitialiser {
+    constructor() {
+
+    }
+}
+
+class GameStacks {
+    constructor() {
+
+    }
+}
+
+class Stack {
+    constructor() {
+
+    }
+}
+
